7628f22e49964e7a34d989aaafd146ae277a3e82
Kevin Jonathan <kevinjonathan@istts.onmicrosoft.com>
2023-12-01 13:08:38 +0700

    Added initial Activites Needed

---
+++ app/src/main/java/com/capstone/foodresq/ui/list/ListActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.foodresq.ui.list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.foodresq.R
+
+class ListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
