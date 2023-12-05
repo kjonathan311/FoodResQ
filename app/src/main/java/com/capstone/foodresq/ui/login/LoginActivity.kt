d5c6b22374eff11979b26305654f3a1c6e912460
Annisa Intan Nuraini <122970017+athyathanasia01@users.noreply.github.com>
2023-12-05 23:58:06 +0700

    update register and detail interface

--- app/src/main/java/com/capstone/foodresq/ui/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/foodresq/ui/login/LoginActivity.kt
@@ -1,12 +1,118 @@
 package com.capstone.foodresq.ui.login
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.core.view.isVisible
 import com.capstone.foodresq.R
+import com.capstone.foodresq.databinding.ActivityLoginBinding
+import com.capstone.foodresq.ui.main.MainActivity
+import com.capstone.foodresq.ui.register.RegisterActivity
+import com.capstone.foodresq.utils.Utils
 
 class LoginActivity : AppCompatActivity() {
+
+    private var _binding : ActivityLoginBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpView()
+        setButtonLoginEnable()
+        onTextInputChanged()
+        buttonLoginHandler()
+        setRegisterHandler()
     }
+
+    private fun setButtonLoginEnable(){
+        val email = binding.textInputEditEmail.text.toString()
+        val password = binding.textInputEditPassword.text.toString()
+
+        binding.btnLogin.isEnabled = email.isNotEmpty() && password.isNotEmpty() && Utils.isValidEmail(email) && Utils.isValidPassword(password)
+    }
+
+    private fun onTextInputChanged(){
+        binding.textInputEditEmail.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                setButtonLoginEnable()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        binding.textInputEditPassword.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                setButtonLoginEnable()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+    }
+
+    private fun buttonLoginHandler(){
+        if (binding.btnLogin.isEnabled){
+            binding.btnLogin.setOnClickListener {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                Handler(Looper.getMainLooper()).postDelayed({
+                    showLoading(true)
+                    startActivity(intent)
+                    showLoading(false)
+                }, 3000)
+            }
+        }
+    }
+
+    private fun setRegisterHandler(){
+        binding.txtNavigateToRegister.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            Handler(Looper.getMainLooper()).postDelayed({
+                showLoading(true)
+                binding.txtNavigateToRegister.setTextColor(resources.getColor(R.color.color_text_4))
+                startActivity(intent)
+                showLoading(false)
+                binding.txtNavigateToRegister.setTextColor(resources.getColor(R.color.color_palette_5))
+            }, 2000)
+        }
+    }
+
+    private fun showLoading(load : Boolean){
+        binding.layoutLoading.progressBar.isVisible = load == true
+    }
+
+    private fun setUpView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
 }
--- app/src/main/java/com/capstone/foodresq/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/capstone/foodresq/ui/register/RegisterActivity.kt
@@ -1,12 +1,127 @@
 package com.capstone.foodresq.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import com.capstone.foodresq.R
+import com.capstone.foodresq.databinding.ActivityRegisterBinding
+import com.capstone.foodresq.ui.login.LoginActivity
+import com.capstone.foodresq.utils.Utils
 
 class RegisterActivity : AppCompatActivity() {
+
+    private var _binding : ActivityRegisterBinding? = null
+    private val binding get() = _binding!!
+    private var itemSelected : String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        _binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val dropDown = binding.dropdownUser
+
+        val typeUser = resources.getStringArray(R.array.type_user)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, typeUser)
+        dropDown.setAdapter(arrayAdapter)
+        dropDown.onItemClickListener = AdapterView.OnItemClickListener{adapterView, view, position, l ->
+            itemSelected = adapterView.getItemAtPosition(position).toString()
+        }
+
+        setButtonRegisterEnabled()
+        onTextFieldChanged()
+        setLoginHandler()
+        buttonRegisterHandler()
+    }
+
+    private fun onTextFieldChanged(){
+        binding.textInputEditEmail.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                binding.btnRegister.isEnabled = false
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                setButtonRegisterEnabled()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        binding.textInputEditPassword.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                binding.btnRegister.isEnabled = false
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                setButtonRegisterEnabled()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+    }
+
+    private fun setButtonRegisterEnabled() {
+        val email = binding.textInputEditEmail.text.toString()
+        val password = binding.textInputEditPassword.text.toString()
+
+        binding.btnRegister.isEnabled = email.isNotEmpty() && password.isNotEmpty() && Utils.isValidEmail(email) && Utils.isValidPassword(password)
+    }
+
+    private fun buttonRegisterHandler(){
+        binding.btnRegister.setOnClickListener {
+            if (itemSelected == null){
+                binding.dropDownTypeUser.error = "Please define your user type"
+            } else{
+                val message = "Your new account already set. Let's get in!"
+                val title = "Success"
+                makeAlert(message, title)
+
+                binding.textInputEditEmail.text = null
+                binding.textInputEditPassword.text = null
+                itemSelected = null
+            }
+        }
+    }
+
+    private fun setLoginHandler() {
+        binding.txtNavigateToLogin.setOnClickListener {
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            Handler(Looper.getMainLooper()).postDelayed({
+                showLoading(true)
+                binding.txtNavigateToLogin.setTextColor(resources.getColor(R.color.color_text_4))
+                startActivity(intent)
+                showLoading(false)
+                binding.txtNavigateToLogin.setTextColor(resources.getColor(R.color.color_palette_5))
+            }, 3000)
+
+        }
+    }
+
+    private fun makeAlert(message : String, title : String){
+        AlertDialog.Builder(this)
+            .setMessage(message)
+            .setTitle(title)
+            .setPositiveButton("OK"){dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun showLoading(load : Boolean){
+        binding.layoutLoading.progressBar.isVisible = load == true
     }
 }
